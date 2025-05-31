71f314dd9d788d2a70bcc880153b405b03c3dd29
Bruno Borges <seu-email@gmail.com>
2025-05-31 10:45:05 -0300

    Corrige execução final do Flask para Render

--- app.py
+++ app.py
@@ -185,10 +185,6 @@ def criar_usuario():
         flash('Usuário já existe.')
     conn.close()
     return redirect(url_for('login'))
-
 if __name__ == '__main__':
-    app.run(debug=True)
-    
-    if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
 
